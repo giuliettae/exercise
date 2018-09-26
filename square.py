58dd44b6b8a6efb6c64ea891efbf4515f006cc3b
giulia paparo <giulia.paparo@gmail.com>
2018-09-26 22:06:18 +0200

    now simpler

--- square.py
+++ square.py
@@ -1,9 +1,11 @@
 import turtle
-turtle.forward(30)
-turtle.left(90)
-turtle.forward(30)
-turtle.left(90)
-turtle.forward(30)
-turtle.left(90)
-turtle.forward(30)
+i=0
+for i in range (0,3):
+    turtle.forward(30)
+    turtle.left(90)
+    i+=1
+
+for i in range (4,5):
+    turtle.forward(30)
+
 turtle.exitonclick()
