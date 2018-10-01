303ee7cf3ee501dcccaeb074f95d538a9fe83d1e
giulia paparo <giulia.paparo@gmail.com>
2018-10-01 20:29:38 +0200

    no need to iterate on a for function

--- square.py
+++ square.py
@@ -1,11 +1,7 @@
 import turtle
-i=0
-for i in range (0,3):
-    turtle.forward(30)
-    turtle.left(90)
-    i+=1
 
-for i in range (4,5):
+for i in range (0,4):
+    turtle.left(90)
     turtle.forward(30)
 
 turtle.exitonclick()
